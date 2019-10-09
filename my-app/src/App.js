453092a4691688a6f96995de571836301cd258ea
dacoquima <danimalistica@gmail.com>
2019-10-09 17:21:49 -0300

    Se inicializa React

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Estoy en contrucción :3
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
