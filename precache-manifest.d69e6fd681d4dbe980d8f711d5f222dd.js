32a9360b9c96028e199a906d74c39829bce3dfb5
dacoquima <danimalistica@gmail.com>
2019-10-09 17:30:00 -0300

    Updates

---
+++ precache-manifest.d69e6fd681d4dbe980d8f711d5f222dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91128dea502b298109e6702c70fd3bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397f9dc3a6fbb58386ef",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31b29b26bc979ad0940b",
+    "url": "/static/js/2.1e8b7724.chunk.js"
+  },
+  {
+    "revision": "397f9dc3a6fbb58386ef",
+    "url": "/static/js/main.f1dc6e3a.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
