20604b36a455d4aecf18307ab372b6520030ffde
dacoquima <danimalistica@gmail.com>
2019-10-09 17:31:40 -0300

    Updates

---
+++ precache-manifest.651f7f2bde2f331146afb4879b097ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117d9a52468b488096a15d04cbfcb8fd",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "9a65e4b0b48158857c11",
+    "url": "/Portafolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31b29b26bc979ad0940b",
+    "url": "/Portafolio/static/js/2.1e8b7724.chunk.js"
+  },
+  {
+    "revision": "9a65e4b0b48158857c11",
+    "url": "/Portafolio/static/js/main.e8770cb4.chunk.js"
+  },
+  {
+    "revision": "933e727691ac6e8a4107",
+    "url": "/Portafolio/static/js/runtime-main.8716fc4f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d69e6fd681d4dbe980d8f711d5f222dd.js"
+  "/Portafolio/precache-manifest.651f7f2bde2f331146afb4879b097ec1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portafolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
